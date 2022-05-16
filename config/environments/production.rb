4f23c876394bf9c6fa66a2051df9251d66067f91
Kirill Ilyin <cuurjol@gmail.com>
2022-05-17 01:07:15 +0300

    Change Mailtrap email server to Google

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -102,11 +102,11 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: ENV['APP_PRODUCTION_HOST'] }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    user_name: ENV['MAILTRAP_USER_NAME'],
-    password: ENV['MAILTRAP_PASSWORD'],
-    address: ENV['MAILTRAP_ADDRESS'],
-    domain: ENV['MAILTRAP_DOMAIN'],
-    port: ENV['MAILTRAP_PORT'],
-    authentication: :plain
+    user_name: ENV['GOOGLE_SMTP_USER_NAME'],
+    password: ENV['GOOGLE_SMTP_PASSWORD'],
+    address: 'smtp.gmail.com',
+    port: 587,
+    authentication: :plain,
+    enable_starttls_auto: true
   }
 end
