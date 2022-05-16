5c4a582969705ce155613e0c40b5203ed2b75f4c
Kirill Ilyin <cuurjol@gmail.com>
2022-05-16 23:06:37 +0300

    Add Mailtrap settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,4 +98,14 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    user_name: ENV['MAILTRAP_USER_NAME'],
+    password: ENV['MAILTRAP_PASSWORD'],
+    address: ENV['MAILTRAP_ADDRESS'],
+    domain: ENV['MAILTRAP_DOMAIN'],
+    port: ENV['MAILTRAP_PORT'],
+    authentication: :cram_md5
+  }
 end
